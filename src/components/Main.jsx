66c1e027cac22a6c0c9dbb6b2da3f5bb9a36c2f0
Chris L <chris@larsonwerks.com>
2023-09-01 17:20:42 -0700

    updated project fields and anchors

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,27 +12,55 @@ function Main() {
         </aside>
         <article id="work">
                 <section className="project" id="surf-report">
-                    <a href="https://surf-report-app-7b1dc82c323b.herokuapp.com/" target="_blank" rel="noreferrer"><p>surf report</p> </a>
+                    <p>
+                    <a href="https://surf-report-app-7b1dc82c323b.herokuapp.com/" target="_blank" rel="noreferrer">surf report</a>
+                        <br/>
+                        <a href="https://github.com/chlarson74/javascript-quiz" target="_blank" rel="noreferrer">~ view repo</a>
+                    </p>
                 </section>
                 <section className="project" id="bb-quote">
-                    <a href="https://jarell-chinn.github.io/BreakingBad_Dashboard/" target="_blank" rel="noreferrer"><p>breaking bad dashboard</p> </a>
+                    <p>
+                        <a href="https://jarell-chinn.github.io/BreakingBad_Dashboard/" target="_blank" rel="noreferrer">breaking bad dashboard</a>
+                        <br/>
+                        <a href="https://github.com/chlarson74/javascript-quiz" target="_blank" rel="noreferrer">~ view repo</a>
+                    </p>
                 </section>
 
-                <section className="project" id="seo">
-                    <a href="https://chlarson74.github.io/refactor-homework/" target="_blank" rel="noreferrer"><p>seo services</p> </a>
+                <section className="project" id="seo">                   
+                    <p>
+                        <a href="https://chlarson74.github.io/refactor-homework/" target="_blank" rel="noreferrer">seo services</a>
+                        <br/>
+                        <a href="https://github.com/chlarson74/javascript-quiz" target="_blank" rel="noreferrer">~ view repo</a>
+                    </p>
                 </section>
            
                 <section className="project" id="pike-street">
-                    <p>pike street services</p>
+                    <p>
+                        <a href="https://chlarson74.github.io/javascript-quiz/" target="_blank" rel="noreferrer">pool hall quiz</a>
+                        <br/>
+                        <a href="https://github.com/chlarson74/javascript-quiz" target="_blank" rel="noreferrer">~ view repo</a>
+                    </p>
                 </section>
                 <section className="project" id="smith-tower">
-                    <p>smith tower technologies</p>
+                    <p>
+                        <a href="https://imperial-text-deeacf34c03b.herokuapp.com/" target="_blank" rel="noreferrer">text editor app</a>
+                        <br/>
+                        <a href="https://github.com/chlarson74/text-editor" target="_blank" rel="noreferrer">~ view repo</a>
+                    </p>
                 </section>
                 <section className="project" id="space-needle">
-                    <p>space needle engineering</p>
+                    <p>
+                        <a href="https://chlarson74.github.io/weather-app/" target="_blank" rel="noreferrer">weather app</a>
+                        <br/>
+                        <a href="https://github.com/chlarson74/weather-app" target="_blank" rel="noreferrer">~ view repo</a>
+                    </p>
                 </section>
                 <section className="project" id="flag">
-                    <p>red flag photography</p>
+                    <p>
+                        <a href="#" target="_blank" rel="noreferrer">red flag photography</a>
+                        <br/>
+                        <a href="#" target="_blank" rel="noreferrer">~ coming soon</a>
+                    </p>
                 </section>
                 <section className="project" id="augmented">
                     <p>augmented reality</p>
